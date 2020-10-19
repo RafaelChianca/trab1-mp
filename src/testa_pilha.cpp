f00fd9016c87fc27ac643770642f145fc4dd668c
Rafael Chianca <rafachianca@gmail.com>
2020-10-18 22:07:31 -0300

    Adds tests for DeleteStack

--- src/testa_pilha.cpp
+++ src/testa_pilha.cpp
@@ -22,7 +22,7 @@ TEST_CASE( "Tests for push and pop", "[pushpop]" ) {
 
   Push(&stack, 40);
   Push(&stack, 50);
-  REQUIRE( Top(stack) == 40 ); 
+  REQUIRE( Top(stack) == 40 );
 }
 
 TEST_CASE( "Tests for stack size", "[size]" ) {
@@ -73,4 +73,22 @@ TEST_CASE( "Tests for SetSize", "[setsize]" ) {
   Push(&stack, 20);
   Push(&stack, 30);
   REQUIRE( IsFull(stack) == 1 );
+}
+
+TEST_CASE( "Tests for DeleteStack", "[delete]" ) {
+  
+  StackNode *stack = CreateStack(3);
+
+  REQUIRE( IsEmpty(stack) == 1 );
+
+  Push(&stack, 10);
+  Push(&stack, 20);
+  Push(&stack, 30);
+  REQUIRE( IsFull(stack) == 1 );
+  REQUIRE( StackSize(stack) == 3 );
+
+  DeleteStack(&stack);
+  REQUIRE( StackSize(stack) == 0 );
+  REQUIRE( IsFull(stack) == 0 );
+  REQUIRE( IsEmpty(stack) == 1 );
 }

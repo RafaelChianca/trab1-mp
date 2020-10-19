95d807db2dc04b05104c8a6a601a33e2f469080e
Rafael Chianca <rafachianca@gmail.com>
2020-10-18 22:39:06 -0300

    Changes file name

--- src/testa_pilha.c
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch.hpp>
-
-TEST_CASE( "Creating Stacks", "[CreateStack]" ) {
-    REQUIRE( CreateStack(1) == 1 );
-    // REQUIRE( CreateStack(2)->itemType == 2 );
-    // REQUIRE( CreateStack(3)->itemType == 6 );
-    // REQUIRE( CreateStack(10)->itemType == 3628800 );
-}

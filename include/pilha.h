27996d4e851282ab749f3ca25967a29d53895c0d
Rafael Chianca <rafachianca@gmail.com>
2020-10-18 22:38:38 -0300

    Refactors after tests

--- include/pilha.h
+++ include/pilha.h
@@ -1,12 +1,24 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 struct StackNode {
 	int itemType;
 	struct StackNode* next;
+	int top; 
+    unsigned capacity; 
+    int* array;
+};
+
+struct ItemType {
+	unsigned data;
 };
 
 struct StackNode* CreateStack(int);
-void DeleteStack(struct StackNode*);
+void DeleteStack(struct StackNode**);
 int StackSize(struct StackNode*);
 int SetSize(struct StackNode**, int);
+int IsFull(struct StackNode*);
 int IsEmpty(struct StackNode*);
 void Push(struct StackNode**, int);
 int Pop(struct StackNode**);
